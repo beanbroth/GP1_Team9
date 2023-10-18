2aeb290e50685d38103ea2b5dd5e931d8929390d
Filip Juntti Åkerlund <72560150+filipakerlund@users.noreply.github.com>
2023-10-18 18:54:19 +0200

    Restructured project

--- Assets/Filip[InputSystem]/Scripts/S_PlayerMovement.cs
+++ Assets/Filip[InputSystem]/Scripts/S_PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class S_PlayerMovement : MonoBehaviour
 {
     S_PlayerControls playerControls;
 
