dc9d679e4c32318b19a5a2e20edb92a6f5fc86dc
TheRealAphiwit <aphiwit.lekphet@edu.futuregames.nu>
2023-10-19 14:50:39 +0200

    Made enemy spawner
    Made a enemy spawner.

--- Assets/[Version1Systems]/Aphiwit[EnemySystem]/EnemyScripts/S_EnemySpawnManager.cs
+++ Assets/[Version1Systems]/Aphiwit[EnemySystem]/EnemyScripts/S_EnemySpawnManager.cs
@@ -5,37 +5,56 @@ using UnityEngine;
 
 public class S_EnemySpawnManager : MonoBehaviour
 {
+    public LayerMask groundLayerMask;
+
     // This script will manage the enemies and their spawn rate. 
     [SerializeField] GameObject[] _enemyPrefabList;
+    [SerializeField] Vector3 _spawnPoint;
     [SerializeField] float _spawnRate;
     [SerializeField] float _spawnPointRange;
+    [SerializeField] bool _spawnPointExist;
     [SerializeField] bool _isSpawning;
-
     
+    private float _spawnTimer;
+
+    S_GeneralMethods generalMethods;
+    // S_GeneralVariables generalVariables;
+
     void Start()
     {
-        
+        _spawnTimer = _spawnRate;
+
+        // generalVariables = gameObject.AddComponent<S_GeneralVariables>();
+        // generalMethods = gameObject.AddComponent<S_GeneralMethods>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GenerateSpawnPoint();
+
+        _spawnTimer -= Time.deltaTime;
+
+        if (_spawnTimer < 0)
+        {
+            _spawnTimer = _spawnRate;
+            Instantiate(_enemyPrefabList[UnityEngine.Random.Range(0, _enemyPrefabList.Length)], _spawnPoint, Quaternion.identity);
+        }
     }
 
-    //private void SearchSpawnPoint()
-    //{
-    //    // Generate a random position to walk to
-    //    float randomZ = UnityEngine.Random.Range(-_spawnPointRange, _spawnPointRange);
-    //    float randomX = UnityEngine.Random.Range(-_spawnPointRange, _spawnPointRange);
+    private void GenerateSpawnPoint()
+    {
+        // Generate a random position to spawn on
+        float randomZ = UnityEngine.Random.Range(-_spawnPointRange, _spawnPointRange);
+        float randomX = UnityEngine.Random.Range(-_spawnPointRange, _spawnPointRange);
 
-    //    _spawnPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        _spawnPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-    //        // Check if point generated exists
-    //        if (Physics.Raycast(_spawnPoint, -transform.up, 2f, groundLayerMask))
-    //        {
-    //            _spawnPointSet = true;
-    //        }
-    //}
-
+        // Check if point generated exists
+        if (!Physics.Raycast(_spawnPoint, -transform.up, 2f, groundLayerMask))
+        {
+            _spawnPointExist = true;
+            Debug.Log("Spawn point found!");
+        }
+    }
 }
