aa0c0896bdc169866c809724aff62973f5389833
Filip Juntti Åkerlund <72560150+filipakerlund@users.noreply.github.com>
2023-10-24 15:40:47 +0200

    Updated Pausemenu
    to have the turn functions working, now you can switch scene

--- Assets/[Version3Systems]---(AcitveFolder)/Programming/Filip [Pause Menu]/Scripts/S_PauseMenu.cs
+++ Assets/[Version3Systems]---(AcitveFolder)/Programming/Filip [Pause Menu]/Scripts/S_PauseMenu.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class S_PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject PauseMenu; // Reference to the pause screen.
+
     private S_PlayerControls playerControls; // Reference to player inputs.
+
     private bool isPaused = false; // Variable for if the game is paused or not.
 
     private void Awake()
@@ -13,11 +16,27 @@ public class S_PauseMenu : MonoBehaviour
         {
             Pause(); // Subscribe Pause() to the "Pause" input.
         };
+
+        playerControls.Player.Turn.performed += context =>
+        {
+            float turnValue = context.ReadValue<float>();
+
+            if (turnValue == 1f && isPaused)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+
+            if (turnValue == -1f && isPaused)
+            {
+                Pause();
+            }
+        };
     }
 
     private void Pause()
     {
         isPaused = !isPaused; // Switch the bool, paused or not.
+
         if (isPaused) // If paused show the pause screen and pause time.
         {
             PauseMenu.SetActive(true);
