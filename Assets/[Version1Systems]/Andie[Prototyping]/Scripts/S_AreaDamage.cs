290703c9029ea5f17474286bdaa9b454c05e61e0
Filip Juntti Åkerlund <72560150+filipakerlund@users.noreply.github.com>
2023-10-31 16:58:43 +0100

    radiation field
    - vfx works (don't cast shadows)
    - code works (matches damage area  with the visual area)
    - locked rotation

--- Assets/[Version1Systems]/Andie[Prototyping]/Scripts/S_AreaDamage.cs
+++ Assets/[Version1Systems]/Andie[Prototyping]/Scripts/S_AreaDamage.cs
@@ -8,20 +8,27 @@ public class S_RadiationAreaController : MonoBehaviour
     public GameObject areaObject;
     public int damage;
     public float timeBetweenAttacks;
-    private Renderer _renderer;
-    [SerializeField] private Color flashColor;
-    private Color originalColor;
+    Quaternion startRotation;
+    //private Renderer _renderer;
+    //[SerializeField] private Color flashColor;
+    //private Color originalColor;
 
-    private float timer;
+    //private float timer;
 
     private void Start()
     {
-        _renderer = areaObject.GetComponent<Renderer>();
-        originalColor = _renderer.material.color;
+        //_renderer = areaObject.GetComponent<Renderer>();
+        //originalColor = _renderer.material.color;
         InvokeRepeating("DamageTick", 0.0f, timeBetweenAttacks);
+        areaObject.transform.localScale = new Vector3(1, 1, 1) * radius/3;
+        startRotation = areaObject.transform.rotation; 
+    }
+    private void Update()
+    {
+        areaObject.transform.rotation = startRotation;
     }
 
-    private void Update()
+    /*private void Update()
     {
         // Constantly change color
         timer += Time.deltaTime;
@@ -29,7 +36,7 @@ public class S_RadiationAreaController : MonoBehaviour
         _renderer.material.color = Color.Lerp(originalColor, flashColor, lerpValue);
 
         areaObject.transform.localScale = new Vector3(radius * 2, areaObject.transform.localScale.y, radius * 2);
-    }
+    }*/
 
     private void DamageTick()
     {
