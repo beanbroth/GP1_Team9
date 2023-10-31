4a7ce6680f47d808555b0b34816d8ab493c18109
jakobAtFG <144691948+jakobAtFG@users.noreply.github.com>
2023-10-31 11:13:13 +0100

    Added splash screen sound

--- Assets/[Version4Systems]---(AcitveFolder)/Jakob[Mixed]/UIAnimation/S_SplashScreen.cs
+++ Assets/[Version4Systems]---(AcitveFolder)/Jakob[Mixed]/UIAnimation/S_SplashScreen.cs
@@ -11,9 +11,11 @@ public class S_SplashScreen : MonoBehaviour
     UIScaleBounce bouncer;
     [SerializeField] Animator rotationAnimator;
     S_SceneTransition sceneTransition;
+    AudioSource audioSource;
     private void Awake()
     {
         sceneTransition = FindFirstObjectByType<S_SceneTransition>();
+        audioSource = GetComponent<AudioSource>();
     }
     void Start()
     {
@@ -24,6 +26,7 @@ public class S_SplashScreen : MonoBehaviour
     }
     void PrepareAnimation()
     {
+        audioSource.Play();
         rotationAnimator.SetTrigger("Rotate");
     }
     void PrepareBounce()
